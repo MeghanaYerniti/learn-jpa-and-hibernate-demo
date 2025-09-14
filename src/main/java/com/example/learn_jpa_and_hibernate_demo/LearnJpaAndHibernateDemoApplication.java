c03974c73de0e129b5b6fe22032c38f623574b12
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-09-14 17:19:18 +0530

    Initial commit

---
+++ src/main/java/com/example/learn_jpa_and_hibernate_demo/LearnJpaAndHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_jpa_and_hibernate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learn_jpa_and_hibernate_demo/LearnJpaAndHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_jpa_and_hibernate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
