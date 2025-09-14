a207f3ca44db762b6d383d2f792c9b9d999eaf65
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-09-14 18:47:14 +0530

    Changes

---
+++ src/main/java/com/example/learn_jpa_and_hibernate_demo/course/jdbc/Course.java
@@ -0,0 +1,5 @@
+package com.example.learn_jpa_and_hibernate_demo.course.jdbc;
+
+public class Course {
+
+}
---
+++ src/main/java/com/example/learn_jpa_and_hibernate_demo/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,21 @@
+package com.example.learn_jpa_and_hibernate_demo.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String insert_Query =
+            """
+            insert into course (id, name, author) values (1,'Learn Spring', 'GPT');
+            """;
+    public void insert(Course c) { //can be any method name
+        springJdbcTemplate.update(insert_Query);
+    }
+}
