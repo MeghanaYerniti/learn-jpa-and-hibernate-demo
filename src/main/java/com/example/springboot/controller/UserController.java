ff2cb88995ca1353b6a076a393d94889b4c3f3d6
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-09-19 11:14:12 +0530

    GET and POST methods in RestAPI

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -2,10 +2,11 @@ package com.example.springboot.controller;
 
 import com.example.springboot.entity.UserBean;
 import com.example.springboot.repository.UserDaoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,6 +25,26 @@ public class UserController {
     @GetMapping("/users/{id}")
     public UserBean findId(@PathVariable Integer id) {
         return userDaoService.findOne(id);
+    }
+
+//    @PostMapping("/users")
+//    public void createUser(@RequestBody UserBean userBean) {
+//        userDaoService.addUser(userBean);
+//    }
+
+//    // returning response status
+//    @PostMapping("/users")
+//    public ResponseEntity<Object> createUser(@RequestBody UserBean userBean) {
+//        userDaoService.addUser(userBean);
+//        return ResponseEntity.created(null).build(); // send 201 as response
+//    }
+
+    // returning url and Id
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody UserBean userBean) {
+        UserBean savedUser = userDaoService.addUser(userBean);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/users/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build(); // send 201 as response
     }
 
 }
--- src/main/java/com/example/springboot/repository/UserDaoService.java
+++ src/main/java/com/example/springboot/repository/UserDaoService.java
@@ -2,6 +2,7 @@ package com.example.springboot.repository;
 
 
 import com.example.springboot.entity.UserBean;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -11,21 +12,33 @@ import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
+
+    private static int count = 0;
     private static final List<UserBean> users = new ArrayList<>();
     static{
-        users.add(new UserBean(1,"Abhi", LocalDate.now().minusYears(30)));
-        users.add(new UserBean(2,"Likhitha", LocalDate.now().minusYears(21)));
-        users.add(new UserBean(3,"Eve", LocalDate.now().minusYears(28)));
+        users.add(new UserBean(++count,"Abhi", LocalDate.now().minusYears(30)));
+        users.add(new UserBean(++count,"Likhitha", LocalDate.now().minusYears(21)));
+        users.add(new UserBean(++count,"Eve", LocalDate.now().minusYears(28)));
     }
 
+    //get
     public List<UserBean> findAll() {
         return users;
     }
 
+    //get
     public UserBean findOne(Integer id) {
         Predicate<? super UserBean> predicate = user -> {
             return user.getId().equals(id);
         };
         return users.stream().filter(predicate).findFirst().get();
     }
+
+    //post
+    public UserBean addUser(UserBean user) {
+        user.setId(++count);
+        users.add(user);
+        return user;
+    }
+
 }
