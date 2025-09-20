65b226e58bd95bf84411f2d4694f5038450d4e15
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-09-20 23:32:03 +0530

    Versioning Using Request Parameters

--- src/main/java/com/example/springboot/controller/PersonVersionController.java
+++ src/main/java/com/example/springboot/controller/PersonVersionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersionController {
 
+    // versioning using Url
     @GetMapping("/v1/person")
     public PersonV1 GetFirstVersion() {
         return new PersonV1("Abhi");
@@ -17,6 +18,17 @@ public class PersonVersionController {
     @GetMapping("/v2/person")
     public PersonV2 GetSecondVersion() {
         return new PersonV2(new Names("James", "Smith"));
+    }
+
+    // versioning using request parameters
+    @GetMapping(path = "/person", params = "version=1")  // URL : http://localhost:8080/person?version=1
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1 ("Ram");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")  // URL : http://localhost:8080/person?version=1
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Names("Abhi", "Ram"));
     }
 
 }
