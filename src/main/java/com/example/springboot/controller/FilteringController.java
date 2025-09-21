90c4092e77284111c51d0a13ed43d91c4fdfd36e
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-09-21 14:10:53 +0530

    Dynamic Filtering

--- src/main/java/com/example/springboot/controller/FilteringController.java
+++ src/main/java/com/example/springboot/controller/FilteringController.java
@@ -1,6 +1,10 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.entity.FilteringBean;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,17 @@ public class FilteringController {
     @GetMapping("/filtering")
     public FilteringBean filtering() {
         return new FilteringBean("Maya", "M@123", "Vzm");
+    }
+
+    @GetMapping("/filtering-dynamic")
+    public MappingJacksonValue filteringDynamic() {
+        FilteringBean filteringBean = new FilteringBean("Maya", "M@123", "Vzm");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(filteringBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("name", "city");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+        // @JsonFilter("SomeBeanFilter") => specify at bean
     }
 
     // list of bean
--- src/main/java/com/example/springboot/entity/FilteringBean.java
+++ src/main/java/com/example/springboot/entity/FilteringBean.java
@@ -6,12 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
-@JsonIgnoreProperties("city") // at class level => here ignoring city filed
+//@JsonIgnoreProperties("city") // at class level => here ignoring city filed
+@JsonFilter("SomeBeanFilter") // for dynamic filtering
 public class FilteringBean {
 
     private String name;
